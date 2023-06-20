34e252796515348758dc029d7da540d47dc42d7b
Dhirachat Chayaporn <omiejung@gmail.com>
2023-06-20 11:52:02 +0700

    Variable - Scopes

---
+++ walletapp/app.py
@@ -0,0 +1,12 @@
+message2 = 'Sawasdee'
+
+def hello2():
+    message = 'Hello2'
+    print(message)
+def hello():
+    print('Hello World')
+    s = 1
+    print(message2)
+    # print(message) # You cannot access other function's variable
+
+

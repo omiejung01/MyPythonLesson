7d730ce0a1173dc854aaccb5c69dc1e01ec02581
Dhirachat Chayaporn <omiejung@gmail.com>
2023-06-27 09:35:29 +0700

    Class Diagram

--- walletapp/app.py
+++ walletapp/app.py
@@ -1,5 +1,16 @@
 message2 = 'Sawasdee'
 
+class Account:
+    def __init__(self,account_name,opening_balance):
+        self.account_name = account_name
+        self.balance = opening_balance
+    def display(self):
+        #Balance Inquiry
+        return self.account_name + ' ' + f"{self.balance:,.2f}"
+    def setAccountName(self, new_name):
+        self.account_name = new_name
+
+
 def hello2():
     message = 'Hello2'
     print(message)
@@ -7,6 +18,13 @@ def hello():
     print('Hello World')
     s = 1
     print(message2)
+
+    jane_account = Account('Jane',1200.00)
+
+    print(jane_account.account_name)
+    print(jane_account.display())
+
+    jane_account.setAccountName('Jane2')
+    print(jane_account.display())
+
     # print(message) # You cannot access other function's variable
-
-

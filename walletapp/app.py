085b26774481ae166cc6facded87e48386f977e3
Dhirachat Chayaporn <omiejung@gmail.com>
2023-06-29 22:33:18 +0700

    Recursion List String

--- walletapp/app.py
+++ walletapp/app.py
@@ -28,3 +28,62 @@ def hello():
     print(jane_account.display())
 
     # print(message) # You cannot access other function's variable
+
+def sum1(num):
+   sum = 0
+   # [1, 2 , 3, 4, 5]
+   for x in range(1, num+1):
+       sum += x
+   return sum
+
+
+def sum2(num):
+   if num == 0:
+       return 0
+   elif num == 1:
+       return 1
+   else:
+       return num + sum2(num - 1)
+   # 5 + 4 + 3 + 2 + 1
+
+
+def factorial(num):
+   if num == 0:
+       return 1
+   elif num == 1:
+       return 1
+   else:
+       return num * factorial(num - 1)
+   # 5 * 4 * 3 * 2 * 1
+
+
+def run():
+   #print(sum2(10))
+   print(factorial(7))
+
+
+   score1 = 87
+   score2 = 45
+   score3 = 61
+   average = (score1+score2+score3)/3
+
+
+   mylist = [3, 7, 6, 9, 8, 3]
+
+
+   mylist2 = []
+   mylist2.append(3)
+   mylist2.append(7)
+   mylist2.append(6)
+
+
+   C = 'c'
+   mylist3 = ['A','B', C ]
+   #String
+
+
+   mystring = 'This is a book'
+
+
+   print('Number of chars is ', len(mystring))
+   print(len(mylist3))

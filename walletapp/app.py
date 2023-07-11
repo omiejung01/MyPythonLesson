b559e23844d852d90cb9deab43e3d2c863c7fea1
Dhirachat Chayaporn <omiejung@gmail.com>
2023-07-11 09:57:48 +0700

    Python class composition

--- walletapp/app.py
+++ walletapp/app.py
@@ -1,10 +1,28 @@
 
+
+class Weapon:
+    def __init__(self,name,weapon_type):
+        self.__name = name
+        self.__weapon_type = weapon_type
+        self.__level = 1
+    def getName(self):
+        return self.__name
+
+    def getWeapon_type(self):
+        return self.__weapon_type
+
+
 class Character:
-    def __init__(self, name, vision):
+    def __init__(self, name, vision, weapon_type):
+        default_weapon = Weapon('None', 'None')
+
         self.__character_name = name
         self.__vision = vision
         self.__level = 1
+        self.__weapon_type = weapon_type
+        self.__weapon = default_weapon
         # constructor
+
 
     def levelup(self):
         self.__level += 1
@@ -15,24 +33,37 @@ class Character:
     def display2(self):
         return self.__character_name + ' ' + self.__vision + ' Level:' + str(self.__level)
 
+    def display3(self):
+        return self.__character_name + ' ' + self.__vision + ' Level:' + str(self.__level) + ' ' + self.getWeapon().getName()
     def setCharacterName(self, new_name):
         self.__character_name = new_name
 
+    def setWeapon(self, weapon):
+        #print (self.__weapon_type)
+        #print (weapon.getWeapon_type())
+        if self.__weapon_type == weapon.getWeapon_type():
+            self.__weapon = weapon
+            return True
+        else:
+            print (self.__character_name + ' cannot hold the ' + weapon.getName())
+            return False
+
+    def getWeapon(self):
+        return self.__weapon
 
 def run():
 
     print('Python class')
-    barbatos = Character('Venti','Anemo')
+    barbatos = Character('Venti','Anemo','Bow')
     # create instance 'barbatos' from class 'Character'
-    print(barbatos.character_name)
+    amos_bow = Weapon('Amos Bow', 'Bow')
+    barbatos.setWeapon(amos_bow)
+    print(barbatos.display3())
 
-    beelzebub = Character('Ei','Electro')
-    print(beelzebub.character_name)
-    beelzebub.display()
-    beelzebub.setCharacterName('Raiden Shogun')
-    print(beelzebub.display2())
+    beelzebub = Character('Ei','Electro','Polearm')
+    beelzebub.setWeapon(amos_bow)
 
-    #barbatos.level = 20 Logic error
+    engulfing_lightning = Weapon('Engulfing Lightning','Polearm')
+    beelzebub.setWeapon(engulfing_lightning)
 
-    beelzebub.levelup()
-    print(beelzebub.display2())
+    print(beelzebub.display3())

a6ac1740f6fd872accb032535f0cf70739edac78
Dhirachat Chayaporn <omiejung@gmail.com>
2023-07-24 10:13:36 +0700

    Python Range and Date

--- walletapp/app.py
+++ walletapp/app.py
@@ -1,91 +1,34 @@
 from datetime import datetime
-class Account():
-    def __init__(self,account_name,opening_balance):
-        self.__account_name = account_name
-        self.__balance = opening_balance
-        self.__created_date = datetime.now()
-        self.__activated = True
 
-    def display(self):
-        status = 'Deactivated'
-        if self.__activated:
-            status = 'Activated'
-        return self.__account_name + ' - since ' + self.__created_date.strftime("%b %d, %Y") + ' - status: ' + status
+def test_date():
+    x = datetime.now()
 
-    def balanceInquiry(self):
-        # Balance Inquiry
-        return f"{self.__balance:,.2f}"
+    print(x.year)
+    print(x.strftime("%A"))
 
-class CreditCard(Account):
-    def __init__(self, credit, account_name, opening_balance):
-        self.__credit = credit
-        self.__due_date = None
-        self.__extended_limit = 0
-        self.__extended_until_date = None
-        super().__init__(account_name, opening_balance)
+    #Assign date
+    x2 = datetime(2023, 8, 25)
+    print(x2.strftime("%A %d %b %Y"))
 
-    def display(self):
-        # Method override
-        # Polymorphism #1
-        return super().display() + ' credit: ' + f"{self.__credit:,.2f}"
+    #Date length
+    print(x2 - x)
 
+def test_range():
+    data = range(10)
+    for n in data:
+        print(n)
+    print('\n')
 
-def my_sum(args):
-    # input can be string, list of int, or dictionary of int
-    # Polymorphism #2 (data type)
-    sum = 0
-    if type(args) == str:
-        numbers = args.split()
-        for n in numbers:
-            sum += int(n)
+    data2 = range(110,115)
+    for n in data2:
+        print(n)
+    print('\n')
 
-    if type(args) == list:
-        for n in args:
-            sum += n
+    data3 = range(1100, 1125, 5)
+    for n in data3:
+        print(n)
 
-    if type(args) == dict:
-        vals = args.values()
-        for n in vals:
-            sum += n
-
-    return sum
-
-def display_name(firstname, lastname = '', title = ''):
-    # this method can have one, two and three arguments !!
-    # Polymorphism #3 (default argument)
-    if title == '':
-        title = 'Khun'
-
-    if lastname == '':
-        return title + ' ' + firstname + '\n'
-    else:
-        return title + ' ' + firstname + ' ' + lastname + '\n'
-
-def q01():
-    MyAccount1 = Account('Client #1', 5000)
-    print(MyAccount1.display())
-
-    MyAccount2 = CreditCard(20000, 'Client #2', 5000)
-    print(MyAccount2.display())
-def q02():
-    print(my_sum('10 20 30'))
-    print(my_sum([10, 20, 31]))
-    num = {
-        '#1': 10,
-        '#2': 20,
-        '#3': 32
-    }
-    print(my_sum(num))
-
-def q03():
-    print(display_name('Albedo'))
-    print(display_name('Eula', 'Lawrence'))
-    print(display_name('Prinzessin', 'der Verurteilung!','mein Fräuleins'))
 
 def run():
-    """
-    q01()
-    q02()
-    """
-    q03()
-
+    test_date()
+    test_range()
